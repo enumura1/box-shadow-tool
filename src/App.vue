5bb37c25bd94aa8a136f24365db3420cd39857fe
enumura1 <akirayuto0718@gmail.com>
2023-09-09 17:55:56 +0900

    add:container component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>
+  <div id="topContainer">
     <h1>BOX SHADOW 生成ツール</h1>
-
+  </div>
+  <div id="mainContainer">
+    <div id="leftContainer">
+      <div id="cssAttachedSquare"></div>
+    </div>
+    <div id="rightContainer">
+      <div id="settingValueContainer">
+        <h1>設定値</h1>
+      </div>
+    </div>
+  </div>
+  <div id="bottomContainer">
+    <h1>BOX SHADOW 生成ツール</h1>
   </div>
 </template>
 
