f9b93c522292947c0c2571abf33140eded2194fe
enumura1 <akirayuto0718@gmail.com>
2023-09-09 18:39:46 +0900

    update:rightContainer ui

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,21 @@
     <div id="rightContainer">
       <div id="settingValueContainer">
         <h1>設定値</h1>
+        <input class="slider" type="range">
+        <input class="slider" type="range">
+        <input class="slider" type="range">
+        <input class="slider" type="range">
+        <input type="color">
+        <input class="slider" type="range">
+      </div>
+      <div id="codeContainer">
+        <h5>コード</h5>
+        <p>hogehoge:#hogehoge, hgoehoge:#hogehoge, hgoehoge:#hogehoge,</p>
       </div>
     </div>
   </div>
   <div id="bottomContainer">
-    <h1>BOX SHADOW 生成ツール</h1>
+    <h5>@enumura</h5>
   </div>
 </template>
 
