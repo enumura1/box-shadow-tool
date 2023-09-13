a56414a6572c56695b2510ca176eb1742e3f9c8f
enumura1 <akirayuto0718@gmail.com>
2023-09-13 23:59:54 +0900

    add：input ui

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,17 @@
         <div id="settingValueContainer">
           <h1>設定値</h1>
           <input v-model="sliderTop" class="slider" type="range">
-          <input class="slider" type="range">
-          <input class="slider" type="range">
-          <input class="slider" type="range">
-          <input type="color">
-          <input class="slider" type="range">
+          <input v-model="sliderRight" class="slider" type="range">
+          <input v-model="sliderBottom" class="slider" type="range">
+          <input v-model="sliderLeft" class="slider" type="range">
+          <input v-model="sliderTop" type="color">
         </div>
         <div id="codeContainer">
           <h5>コード</h5>
           <p>{{ sliderTop }}</p>
+          <p>{{ sliderRight }}</p>
+          <p>{{ sliderBottom }}</p>
+          <p>{{ sliderLeft }}</p>
         </div>
       </div>
     </div>
@@ -34,6 +36,9 @@ export default {
   data() {
     return {
       sliderTop: "",
+      sliderRight: "",
+      sliderBottom: "",
+      sliderLeft: "",
     };
   },
   methods: {},
