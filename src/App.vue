2c9b51bc4d4973c9aacdf4d11d7dd48020367444
enumura1 <akirayuto0718@gmail.com>
2023-09-13 23:52:08 +0900

    fix:Adapted to vue3 syntax.

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,45 @@
 <template>
-  <div id="topContainer">
-    <h1>BOX SHADOW 生成ツール</h1>
-  </div>
-  <div id="mainContainer">
-    <div id="leftContainer">
-      <div id="cssAttachedSquare"></div>
+  <div id="app">
+    <div id="topContainer">
+      <h1>BOX SHADOW 生成ツール</h1>
     </div>
-    <div id="rightContainer">
-      <div id="settingValueContainer">
-        <h1>設定値</h1>
-        <input class="slider" type="range">
-        <input class="slider" type="range">
-        <input class="slider" type="range">
-        <input class="slider" type="range">
-        <input type="color">
-        <input class="slider" type="range">
+    <div id="mainContainer">
+      <div id="leftContainer">
+        <div id="cssAttachedSquare"></div>
       </div>
-      <div id="codeContainer">
-        <h5>コード</h5>
-        <p>hogehoge:#hogehoge, hgoehoge:#hogehoge, hgoehoge:#hogehoge,</p>
+      <div id="rightContainer">
+        <div id="settingValueContainer">
+          <h1>設定値</h1>
+          <input v-model="sliderTop" class="slider" type="range">
+          <input class="slider" type="range">
+          <input class="slider" type="range">
+          <input class="slider" type="range">
+          <input type="color">
+          <input class="slider" type="range">
+        </div>
+        <div id="codeContainer">
+          <h5>コード</h5>
+          <p>{{ sliderTop }}</p>
+        </div>
       </div>
     </div>
-  </div>
-  <div id="bottomContainer">
-    <h5>@enumura</h5>
+    <div id="bottomContainer">
+      <h5>@enumura</h5>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      sliderTop: "",
+    };
+  },
+  methods: {},
+};
 </script>
+
+<style>
+/* ここにスタイルを追加 */
+</style>
