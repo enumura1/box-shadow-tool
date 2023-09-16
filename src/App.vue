e59c9606f5da4f518ca5ded13e7a6bf6249fd0a2
enumura1 <akirayuto0718@gmail.com>
2023-09-16 17:45:33 +0900

    add:nav-bar, appbar

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,73 @@
 <template>
-  <div id="app">
-    <div id="topContainer">
-      <h1>BOX SHADOW 生成ツール</h1>
-    </div>
-    <div id="mainContainer">
-      <div id="leftContainer">
-        <!-- 動的にbox-shadowプロパティを更新する四角形 -->
-        <div id="cssAttachedSquare" :style="boxShadowStyle"></div>
-      </div>
-      <div id="rightContainer">
-        <div id="settingValueContainer">
-          <h1>設定値</h1>
-          <input v-model="sliderTop" class="slider" type="range" min="-50" max="50">
-          <input v-model="sliderRight" class="slider" type="range" min="-50" max="50">
-          <input v-model="sliderBlurRadius" class="slider" type="range" min="0" max="100">
-          <input v-model="sliderSpreadRadius" class="slider" type="range" min="-50" max="100">
-          <input v-model="sliderTransparency" class="slider" type="range" min="0" max="1" step="0.01">
-          <input type="color">
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" style="background-color: #1a1e2e; color: white;">
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar elevation="9" style="background-color: #1a1e2e; color: white;">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+      <template v-slot:append>
+        <v-btn>
+          hoge
+        </v-btn>
+
+        <v-btn>
+          <v-icon>
+            mdi-home
+          </v-icon>
+        </v-btn>
+      </template>
+    </v-app-bar>
+
+    <v-main>
+      <div id="app">
+        <div id="topContainer">
+          <h1>BOX SHADOW 生成ツール</h1>
         </div>
-        <div id="codeContainer">
-          <h5>CSSコード</h5>
-          <p class="generatedCssCode">box-shadow: {{ sliderTop }}px {{ sliderRight }}px {{ sliderBlurRadius }}px
-            {{ sliderSpreadRadius }}px {{ sliderTransparency }}</p>
+        <div id="mainContainer">
+          <div id="leftContainer">
+            <!-- 動的にbox-shadowプロパティを更新する四角形 -->
+            <div id="cssAttachedSquare" :style="boxShadowStyle"></div>
+          </div>
+          <div id="rightContainer">
+            <div id="settingValueContainer">
+              <h1>設定値</h1>
+              <input v-model="sliderTop" class="slider" type="range" min="-50" max="50">
+              <input v-model="sliderRight" class="slider" type="range" min="-50" max="50">
+              <input v-model="sliderBlurRadius" class="slider" type="range" min="0" max="100">
+              <input v-model="sliderSpreadRadius" class="slider" type="range" min="-50" max="100">
+              <input v-model="sliderTransparency" class="slider" type="range" min="0" max="1" step="0.01">
+              <input type="color">
+            </div>
+            <div id="codeContainer">
+              <h5>CSSコード</h5>
+              <p class="generatedCssCode">box-shadow: {{ sliderTop }}px {{ sliderRight }}px {{ sliderBlurRadius }}px
+                {{ sliderSpreadRadius }}px {{ sliderTransparency }}</p>
+            </div>
+          </div>
+        </div>
+        <div id="bottomContainer">
+          <h5>@enumura</h5>
         </div>
       </div>
-    </div>
-    <div id="bottomContainer">
-      <h5>@enumura</h5>
-    </div>
-  </div>
+    </v-main>
+
+  </v-app>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(null)
+</script>
 
 <script>
 export default {
   data() {
     return {
+      drawer: null,
       sliderTop: 0,
       sliderRight: 0,
       sliderBlurRadius: 30,
@@ -50,5 +83,5 @@ export default {
     },
   },
   methods: {},
-};
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 外部ファイルのインポート
+import './assets/main.css'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+// Vuetifyの設定を作成します
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
