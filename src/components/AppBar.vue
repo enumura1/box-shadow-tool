03de28380f26e707e23a0f0fca72c037e0991661
enumura1 <akirayuto0718@gmail.com>
2023-09-25 01:28:24 +0900

    update:right colmn container boder-radius

---
+++ src/components/AppBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-app-bar elevation="9" style="background-color: #1a1e2e; color: aliceblue;">
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-app-bar-title>BOX SHADOW 生成ツール</v-app-bar-title>
+        <template v-slot:append>
+        </template>
+    </v-app-bar>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(false);
+
+const toggleDrawer = () => {
+    drawer.value = !drawer.value;
+};
+
+</script>
